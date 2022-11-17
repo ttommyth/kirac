60beb7be917b74a21fbdf1fe3882754f35489c63
ttommyth <ttommyth@gmail.com>
2022-11-17 23:33:26 +0800

    feat(modifier): added mod_stat autocomplete

--- src/commands/modifier/index.ts
+++ src/commands/modifier/index.ts
@@ -1,16 +1,21 @@
 import { ActionRow, ActionRowBuilder, APIApplicationCommandOptionChoice, APIEmbedField, ButtonBuilder, ButtonStyle, CommandInteractionOption, ComponentType, EmbedBuilder, InteractionCollector, MessageActionRowComponent, ModalActionRowComponentBuilder, ModalBuilder, SelectMenuBuilder, SlashCommandBuilder, SlashCommandIntegerOption, TextInputBuilder, TextInputStyle } from "discord.js";
-import { last, minBy } from "lodash";
+import { filter, last, minBy, remove } from "lodash";
 import { StructuredCommand } from "../../types/commands";
 import { ChromaticOptions, ChromaticResult, prebuiltEngine } from '@src/services/chromatic/engine';
 import { genTableImage } from "@src/services/chromatic/tableImage";
-import { availableItemType, availableMods, itemTypeMiniSearch } from "@src/services/modifier";
+import { availableItemType, availableMods, itemTags, itemTypeMiniSearch, itemTypePrefix, itemTypeSuffix, statMiniSearch, statToMod } from "@src/services/modifier";
 
 
 export const modifierCommand: StructuredCommand = async (interaction)=>{
-  console.debug(interaction.options)
-  console.debug(availableItemType)
-  console.debug(availableMods.length)
-  await interaction.reply("-")
+  const statId = interaction.options.data.find(it=>it.name==="mod_stat")?.value?.toString();
+  if(statId){
+    //process with stat id lookup
+
+    await interaction.reply(JSON.stringify(statToMod[statId]))
+  }else{
+
+    await interaction.reply("-")
+  }
 }
 
 modifierCommand.onComponentInteraction=async(interaction)=>{
@@ -25,9 +30,60 @@ modifierCommand.onAutoComplete= async(interaction)=>{
   switch(focusedOption.name){
   case "item_type":
     {
-      await interaction.respond(
-        itemTypeMiniSearch.search(focusedOption.value, {fuzzy:0.2}).slice(0,25).map(it=>({name:it.label, value:""+it.id}))
-      )
+      if(focusedOption.value?.length<2){
+        await interaction.respond(
+          Object.entries(itemTags).filter(it=>it[1].length>4).sort(it=>it[1].length).map(it=>it[0]).slice(0,25).map(it=>({name:it, value:""+it}))
+        )
+      }else{
+        
+        await interaction.respond(
+          itemTypeMiniSearch.search(focusedOption.value, {fuzzy:0.2 }).slice(0,25).map(it=>({name:it.label, value:it.label}))
+        )
+      }
+    }
+    break;
+  case "mod_stat":
+    {
+      const itemType = interaction.options.data.find(it=>it.name=="item_type")?.value?.toString();
+      const itemAttribute= interaction.options.data.find(it=>it.name=="item_attribute")?.value?.toString();
+      const itemInfluence= interaction.options.data.find(it=>it.name=="item_influence")?.value?.toString();
+      if(focusedOption.value?.length<2){
+        await interaction.respond(
+          []
+        )
+      }else{
+        
+        await interaction.respond(
+          statMiniSearch.search(
+            {
+              combineWith: 'AND',
+              fields:["string"],
+              fuzzy:0.2,
+              queries:[
+                focusedOption.value,
+              // ...(itemType?[{
+              //   combineWith:"AND",
+              //   fuzzy:1,
+              //   fields:["indexField"],
+              //   queries: [itemType]
+              // }]:[]),
+              // ...(itemAttribute?[{
+              //   combineWith:"AND",
+              //   fuzzy:1,
+              //   fields:["indexField"],
+              //   queries: [itemAttribute]
+              // }]:[]),
+              // ...(itemInfluence?[{
+              //   combineWith:"AND_NOT",
+              //   fuzzy:1,
+              //   fields:["indexField"],
+              //   queries:  remove(itemTypeSuffix,(it)=>it===itemInfluence)
+              // }]:[]),
+              ] 
+            }
+          ).slice(0,25).map(it=>({name:it.string, value:it.id}))
+        )
+      }
     }
     break;
   }
@@ -37,4 +93,20 @@ modifierCommand.structure = new SlashCommandBuilder()
   .setName("modifier")
   .setDescription("modifier")
   .addStringOption((opt)=>(opt.setName("item_type").setDescription("Item Type")).setAutocomplete(true))
+  .addStringOption((opt)=>(opt.setName("item_attribute").setDescription("Item Attribute Type (e.g. str, str/dex)")).setChoices(...itemTypePrefix.map(it=>(
+    {name:it, value:it}
+  ))
+  ))
+  .addStringOption((opt)=>(opt.setName("item_influence").setDescription("Item Influence Type (e.g. shaper, elder)")).setChoices(...itemTypeSuffix.map(it=>(
+    {name:it, value:it}
+  ))
+  ))
+  .addStringOption((opt)=>(opt.setName("mod_loc").setDescription("Mod Location")).setChoices(...[
+    {name:"prefix", value:"prefix"},
+    {name:"suffix", value:"suffix"},
+    // {name:"implicit", value:"implicit"},
+    // {name:"synthesis", value:"synthesis"},
+    {name:"corrupted", value:"corrupted"},
+  ]))
+  .addStringOption((opt)=>(opt.setName("mod_stat").setDescription("Mod Stat (e.g. Adds (56–87) to (105–160) Chaos Damage)")).setAutocomplete(true))
   .toJSON()
--- src/services/modifier/index.ts
+++ src/services/modifier/index.ts
@@ -3,7 +3,28 @@ import mods from "@assets/repoe/mods.min.json";
 import stat_translations from "@assets/repoe/stat_translations.min.json";
 import tags from "@assets/repoe/tags.json";
 import MiniSearch from "minisearch";
+import { groupBy, truncate } from "lodash";
 
+export type StatCondition={
+  min?:number,
+  max?:number
+}
+export type StatTranslationDetailPerId={
+  "condition": StatCondition,
+  "format": string, 
+  "index_handlers": string[],
+  "string": string
+}
+export type StatTranslationDetail={
+  "condition": StatCondition[],
+  "format": string[], //["#"],
+  "index_handlers": Array<string[]>,
+  "string": string
+}
+export type StatTranslation={
+  "English": StatTranslationDetail[],
+  "ids": string[]
+}
 export type Mods = {
     adds_tags: [];
     domain: string;
@@ -27,7 +48,206 @@ export type Mods = {
       }[],
     type: string
 }
-export const availableMods:{[key:string]:Mods} = Object.entries(mods).filter(it=>it[1].generation_type.endsWith("fix")) as any
-export const availableItemType=tags.map((it,idx)=>({id: idx,label:it}));
+export type TagSearcher={
+ containTags:string[],
+ notContainTags:string[],
+ typeFilter?:(type:string)=>boolean
+}
+
+export const itemTypePrefix = [
+  "str_dex_int",
+  "str_int",
+  "dex_int",
+  "str_dex",
+  "int",
+  "dex",
+  "str",
+]
+export const itemTypeSuffix = [
+  "shaper",
+  "elder",
+  "crusader",
+  "adjudicator",
+  "eyrie",
+  "basilisk"
+]
+export const conditionalFix:{name:string, type:"full_wild"|"prefix_wild"|"suffix_wild"}[] = [
+  {
+    name: "abyss_jewel",
+    type: "full_wild"
+  }, 
+  {
+    name: "leaguestone",
+    type: "prefix_wild"
+  }, 
+  {
+    name: "expansion_jewel",
+    type: "suffix_wild"
+  },
+  {
+    name: "affliction",
+    type: "suffix_wild"
+  },
+  {
+    name: "sentinel",
+    type: "suffix_wild"
+  },
+  {
+    name: "expedition",
+    type: "suffix_wild"
+  },
+  {
+    name: "heist_equipment",
+    type: "suffix_wild"
+  },
+  {
+    name: "memory_line",
+    type: "suffix_wild"
+  },
+  {
+    name: "eldritch_implicit",
+    type: "prefix_wild"
+  }, 
+  {
+    name: "bestiary",
+    type: "suffix_wild"
+  }, 
+  {
+    name: "map",
+    type: "prefix_wild"
+  },  
+  {
+    name: "flask",
+    type: "prefix_wild"
+  }, 
+]
+
+export const availableMods:{[key:string]:Mods} = Object.fromEntries(Object.entries(mods)
+  .filter(it=>it[1].generation_type.endsWith("fix") ||it[1].implicit_tags.length>0 || it[1].generation_type.endsWith("implicit") || it[1].type.endsWith("ForJewel")  ));
+
+
+const invokedStatIds = Object.fromEntries(Object.values(availableMods).flatMap(it=>it.stats).map(it=>[it.id,true]));
+export const invokedStat:{[key:string]:StatTranslationDetailPerId[]} = Object.fromEntries(
+  (stat_translations as StatTranslation[]).filter(it=>it.ids.find(id=>invokedStatIds[id]))
+    .flatMap(it=>it.ids.map((id,idx)=>[id, it.English.map(it=>({
+      string: it.string,
+      index_handlers: it.index_handlers[idx],
+      format: it.format[idx],
+      condition: it.condition[idx],
+    } as StatTranslationDetailPerId))]))
+);
+export const statToMod = Object.entries(availableMods).flatMap(it=>{
+  return it[1].stats.map(stat=>[stat.id, it[0]])
+}).reduce((cur,it)=>(cur[it[0]]= [...(cur[it[0]]??[]), it[1]], cur),{} as {[key:string]: string[]})
+
+
+export const statMiniSearch = new MiniSearch({fields:["string","indexField"],storeFields:["string", "id"] })
+statMiniSearch.addAll(Object.entries(invokedStat)
+  .flatMap(p=>p[1]
+    .map(c=>({
+      string: c.string,
+      id:p[0],
+      indexField: statToMod[p[0]]?.map(modId=>[availableMods[modId].type,availableMods[modId].spawn_weights.filter(it=>it.weight>0).map(it=>it.tag).join(',')])
+    }))))
+
+
+const tagsUsedInMods = Object.values(availableMods).reduce((cur, mod)=>(mod.spawn_weights?.forEach(it=>cur[it.tag]=true), cur), {} as {[key:string]:boolean})
+export const itemTags = groupBy(tags, (it)=>{
+  if(!tagsUsedInMods[it])
+    return 0;
+  let outStr = it;
+  const matchedPrefix  =itemTypePrefix.find(pre=>it.startsWith(pre));
+  const matchedSuffix  =itemTypeSuffix.find(suf=>it.endsWith(suf));
+  if(matchedPrefix)
+    outStr = outStr.replace(matchedPrefix+"_","");
+  if(matchedSuffix)
+    outStr = outStr.replace("_"+matchedSuffix,"");
+  if(!matchedPrefix && !matchedSuffix){
+    const matchedConditionalFix  =conditionalFix.find(fix=>outStr.includes(fix.name));
+    if(matchedConditionalFix){
+      const slices = outStr.split(matchedConditionalFix.name)
+      switch(matchedConditionalFix.type){
+      case "full_wild":
+        outStr = matchedConditionalFix.name;
+        break;
+      case "prefix_wild":
+        outStr = matchedConditionalFix.name+(slices.length>2?slices[1]:"");
+        break;
+      case "suffix_wild":
+        outStr = (slices.length>2?slices[0]:"")+matchedConditionalFix.name;
+        break;
+      }
+    }
+  }
+  return outStr;
+})
+delete itemTags[0]
+
+export const availableItemType=Object.keys(itemTags).map((it,idx)=>({id: idx,label:it}));
+
+
 export const itemTypeMiniSearch = new MiniSearch({fields:["label"],storeFields:["label"] })
 itemTypeMiniSearch.addAll(availableItemType);
+
+export const JewelTypeMapToTagSearcher:{[key:string]:TagSearcher}={
+  "Crimson Jewel":{
+    containTags:[],
+    notContainTags:["not_str"],
+    typeFilter:str=>str.endsWith("ForJewel")
+  },
+  "Viridian Jewel":{
+    containTags:[],
+    notContainTags:["not_dex"],
+    typeFilter:str=>str.endsWith("ForJewel")
+  },
+  "Cobalt Jewel":{
+    containTags:[],
+    notContainTags:["not_int"],
+    typeFilter:str=>str.endsWith("ForJewel")
+  },
+  "Prismatic Jewel":{
+    containTags:["default"],
+    notContainTags:[],
+    typeFilter:str=>str.endsWith("ForJewel")
+  },
+  "Murderous Eye Jewel":{
+    containTags:["abyss_jewel_melee"],
+    notContainTags:[""],
+    typeFilter:str=>str.startsWith("AbyssJewel")
+  },
+  "Searching Eye Jewel":{
+    containTags:["abyss_jewel_ranged"],
+    notContainTags:[""],
+    typeFilter:str=>str.startsWith("AbyssJewel")
+  },
+  "Hypnotic Eye Jewel":{
+    containTags:["abyss_jewel_caster"],
+    notContainTags:[""],
+    typeFilter:str=>str.startsWith("AbyssJewel")
+  },
+  "Ghastly Eye Jewel":{
+    containTags:["abyss_jewel_summoner"],
+    notContainTags:[""],
+    typeFilter:str=>str.startsWith("AbyssJewel")
+  },
+  "Timeless Jewel":{
+    containTags:["default"],
+    notContainTags:[],
+    typeFilter:str=>str.endsWith("ForJewel")
+  },
+  "Large Cluster Jewel":{
+    containTags:["expansion_jewel_large"],
+    notContainTags:[],
+    typeFilter:str=>str.startsWith("Affliction")
+  },
+  "Medium Cluster Jewel":{
+    containTags:["expansion_jewel_medium"],
+    notContainTags:[],
+    typeFilter:str=>str.startsWith("Affliction")
+  },
+  "Small Cluster Jewel":{
+    containTags:["expansion_jewel_small"],
+    notContainTags:[],
+    typeFilter:str=>str.startsWith("Affliction")
+  },
+}
